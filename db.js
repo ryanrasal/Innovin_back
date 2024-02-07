5114e2abb10a01279d37ff6a8ec7641f7e9269ba
Ryan Beaujot <ryanbeaujot@air-de-ryan.home>
2024-02-07 17:28:59 +0100

    porkf

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
+console.log("coucou je suis host", process.env.DB_HOST )
+
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
