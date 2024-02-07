b320248c7fecf6bf6b00a61b019f3a1622affcf7
Ryan Beaujot <ryanbeaujot@air-de-ryan.home>
2024-02-07 18:59:34 +0100

    fopjezf

--- test/routes.integration.spec.js
+++ test/routes.integration.spec.js
@@ -2,38 +2,47 @@
 // const request = require("supertest");
 // const app = require("../app");
 // const connection = require("../db");
-const createMessageController = require("../src/controllers/MessageControllers/CreateMessageController");
-const MessageManager = require("../src/models/MessageManager");
+// const createMessageController = require("../src/controllers/MessageControllers/CreateMessageController");
+// const MessageManager = require("../src/models/MessageManager");
 
-jest.mock("../src/models/MessageManager");
+// jest.mock("../src/models/MessageManager");
 
-describe("createMessageController", () => {
-  it("should return status 201 and message on successful insertion", async () => {
-    const req = {
-      body: {
-        email: "test@example.com",
-        subject: "Test",
-        content: "Test message",
-        isRead: false,
-      },
-    };
-    const mockInsertMessage = jest
-      .spyOn(MessageManager, "insertMessage")
-      .mockResolvedValue({ status: 201, message: "message created" });
+// describe("createMessageController", () => {
+//   it("should return status 201 and message on successful insertion", async () => {
+//     const req = {
+//       body: {
+//         email: "test@example.com",
+//         subject: "Test",
+//         content: "Test message",
+//         isRead: false,
+//       },
+//     };
+//     const mockInsertMessage = jest
+//       .spyOn(MessageManager, "insertMessage")
+//       .mockResolvedValue({ status: 201, message: "message created" });
 
-    const res = {
-      json: jest.fn(),
-    };
+//     const res = {
+//       json: jest.fn(),
+//     };
 
-    await createMessageController(req, res);
+//     await createMessageController(req, res);
 
-    expect(mockInsertMessage).toHaveBeenCalledWith(req.body);
-    expect(res.json).toHaveBeenCalledWith({
-      status: 201,
-      message: "message created",
-    });
-  });
+//     expect(mockInsertMessage).toHaveBeenCalledWith(req.body);
+//     expect(res.json).toHaveBeenCalledWith({
+//       status: 201,
+//       message: "message created",
+//     });
+//   });
+// });
+
+// Importation de la fonction à tester
+
+// Test
+test('adds 2 + 2 to equal 4', () => {
+  // Exécution de la fonction à tester
+  expect(sum(2, 2)).toBe(4);
 });
+
 
 // describe("Test routes wines", () => {
 //   beforeEach(async () => {
