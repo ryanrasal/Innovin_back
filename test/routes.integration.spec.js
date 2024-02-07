702c1336ea4b8f416eaf26a4e73d07a058aaa49e
Ryan Beaujot <ryanbeaujot@air-de-ryan.home>
2024-02-07 18:54:25 +0100

    fopjezf

--- test/routes.integration.spec.js
+++ test/routes.integration.spec.js
@@ -1,7 +1,7 @@
 // test/app.integration.spec.js
-const request = require("supertest");
-const app = require("../app");
-const connection = require("../db");
+// const request = require("supertest");
+// const app = require("../app");
+// const connection = require("../db");
 const createMessageController = require("../src/controllers/MessageControllers/CreateMessageController");
 const MessageManager = require("../src/models/MessageManager");
 
@@ -35,94 +35,94 @@ describe("createMessageController", () => {
   });
 });
 
-describe("Test routes wines", () => {
-  beforeEach(async () => {
-    try {
-      const results = await connection.promise().query("SELECT * FROM wine");
-      global.testMessages = results[0];
-    } catch (error) {
-      throw error;
-    }
-  });
+// describe("Test routes wines", () => {
+//   beforeEach(async () => {
+//     try {
+//       const results = await connection.promise().query("SELECT * FROM wine");
+//       global.testMessages = results[0];
+//     } catch (error) {
+//       throw error;
+//     }
+//   });
 
-  it("GET /wines", async () => {
-    try {
-      const response = await request(app)
-        .get("/wines")
-        .expect(200)
-        .expect("Content-Type", /json/);
-      expect(response.body).toEqual(global.testMessages);
-    } catch (error) {
-      throw error;
-    }
-  });
-});
+//   it("GET /wines", async () => {
+//     try {
+//       const response = await request(app)
+//         .get("/wines")
+//         .expect(200)
+//         .expect("Content-Type", /json/);
+//       expect(response.body).toEqual(global.testMessages);
+//     } catch (error) {
+//       throw error;
+//     }
+//   });
+// });
 
 
-describe("Test routes users", () => {
-  beforeEach(async () => {
-    try {
-      const results = await connection.promise().query("SELECT * FROM user");
-      global.testMessages = results[0];
-    } catch (error) {
-      throw error;
-    }
-  });
+// describe("Test routes users", () => {
+//   beforeEach(async () => {
+//     try {
+//       const results = await connection.promise().query("SELECT * FROM user");
+//       global.testMessages = results[0];
+//     } catch (error) {
+//       throw error;
+//     }
+//   });
 
-  it("GET /users", async () => {
-    try {
-      const response = await request(app)
-        .get("/users")
-        .expect(200)
-        .expect("Content-Type", /json/);
-      expect(response.body).toEqual(global.testMessages);
-    } catch (error) {
-      throw error;
-    }
-  });
-});
+//   it("GET /users", async () => {
+//     try {
+//       const response = await request(app)
+//         .get("/users")
+//         .expect(200)
+//         .expect("Content-Type", /json/);
+//       expect(response.body).toEqual(global.testMessages);
+//     } catch (error) {
+//       throw error;
+//     }
+//   });
+// });
 
-describe("Test routes carts", () => {
-  beforeEach(async () => {
-    try {
-      const results = await connection
-        .promise()
-        .query("SELECT * FROM cart WHERE id = 1");
-      global.testMessages = results[0];
-    } catch (error) {
-      throw error;
-    }
-  });
+// describe("Test routes carts", () => {
+//   beforeEach(async () => {
+//     try {
+//       const results = await connection
+//         .promise()
+//         .query("SELECT * FROM cart WHERE id = 1");
+//       global.testMessages = results[0];
+//     } catch (error) {
+//       throw error;
+//     }
+//   });
 
-  it("GET /carts/1", async () => {
-    try {
-      const response = await request(app)
-        .get("/carts/1")
-        .expect(200)
-        .expect("Content-Type", /json/);
-      const exepted = {
-        content: [
-          {
-            cart_wine_id: null,
-            image: null,
-            name: null,
-            origin_country: null,
-            price: null,
-            quantity: null,
-            total: null,
-            wine_id: null,
-            wine_type: null,
-          },
-        ],
-        id: 1,
-        is_order: 0,
-        user_id: 1,
-      };
-      expect(response.body).toEqual(exepted);
-    } catch (error) {
-      throw error;
-    }
-  });
-});
+//   it("GET /carts/1", async () => {
+//     try {
+//       const response = await request(app)
+//         .get("/carts/1")
+//         .expect(200)
+//         .expect("Content-Type", /json/);
+//       const exepted = {
+//         content: [
+//           {
+//             cart_wine_id: null,
+//             image: null,
+//             name: null,
+//             origin_country: null,
+//             price: null,
+//             quantity: null,
+//             total: null,
+//             wine_id: null,
+//             wine_type: null,
+//           },
+//         ],
+//         id: 1,
+//         is_order: 0,
+//         user_id: 1,
+//       };
+//       expect(response.body).toEqual(exepted);
+//     } catch (error) {
+//       throw error;
+//     }
+//   });
+// });
 
 
