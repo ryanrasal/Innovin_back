4ae520a31cbad7b5d33cbf05d899f461e304106c
Ryan Beaujot <ryanbeaujot@air-de-ryan.home>
2024-02-09 19:37:50 +0100

    first commit

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: 3308,
+  host: process.env.MYSQL_HOST,
   port: process.env.DB_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
