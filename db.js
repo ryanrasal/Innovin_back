068bb56cf3b39f8b72edf6afdcb2ed31f3441267
Ryan Beaujot <ryanbeaujot@air-de-ryan.home>
2024-02-09 18:51:28 +0100

    ci/cd

--- db.js
+++ db.js
@@ -2,24 +2,12 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
+  host: "localhost",
+  port: 3308,
+  user: "innovin",
+  password: "innovin",
+  database: "innovin",
 });
 
-console.log(
-  "host:",
-  process.env.MYSQL_HOST,
-  "port:",
-  process.env.DB_PORT,
-  "user:",
-  process.env.MYSQL_USER,
-  "password:",
-  process.env.MYSQL_PASSWORD,
-  "database:",
-  process.env.MYSQL_DB
-);
 
 module.exports = connection;
