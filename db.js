6b1265e7927bd30b669b1e0163955b7ca0ec2bd8
Ryan Beaujot <ryanbeaujot@air-de-ryan.home>
2024-02-09 19:51:36 +0100

    first commit

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
 require("dotenv").config();
 const mysql = require("mysql2");
-const timeout = 30000;
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   port: process.env.DB_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
-  connectTimeout: timeout,
 });
 
 console.log(process.env.MYSQL_HOST, process.env.DB_PORT, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, process.env.MYSQL_DB)
