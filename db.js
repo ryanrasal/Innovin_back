ac1dc884a4133c3ba542b7f13d58de42fe233f11
Ryan Beaujot <ryanbeaujot@air-de-ryan.home>
2024-02-09 18:21:33 +0100

    ci/cd

--- db.js
+++ db.js
@@ -9,4 +9,17 @@ const connection = mysql.createConnection({
   database: process.env.MYSQL_DB,
 });
 
+console.log(
+  "host:",
+  process.env.MYSQL_HOST,
+  "port:",
+  process.env.DB_PORT,
+  "user:",
+  process.env.MYSQL_USER,
+  "password:",
+  process.env.MYSQL_PASSWORD,
+  "database:",
+  process.env.MYSQL_DB
+);
+
 module.exports = connection;
