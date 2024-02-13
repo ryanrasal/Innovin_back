176fd55e57aa6769b8d47f63af0a439df6870d43
Ryan Beaujot <ryanbeaujot@air-de-ryan.home>
2024-02-13 10:36:39 +0100

    ci.yml

--- db.js
+++ db.js
@@ -9,13 +9,26 @@ const connection = mysql.createPool({
   database: process.env.MYSQL_DB,
 });
 
+console.log(
+  "host= ",
+  process.env.MYSQL_HOST,
+  "port= ",
+  process.env.MYSQL_PORT,
+  "user= ",
+  process.env.MYSQL_USER,
+  "password= ",
+  process.env.MYSQL_PASSWORD,
+  "database= ",
+  process.env.MYSQL_DB
+);
+
 // Ajout du message de connexion réussie à la console
 connection.getConnection((err, conn) => {
   if (err) {
-    console.error('Erreur de connexion à la base de données :', err);
+    console.error("Erreur de connexion à la base de données :", err);
     return;
   }
-  console.log('Connexion à la base de données réussie !');
+  console.log("Connexion à la base de données réussie !");
   conn.release();
 });
 
