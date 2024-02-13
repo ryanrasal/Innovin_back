05f57c04728113b71abb01bdc40ee6d8e57dc115
Ryan Beaujot <ryanbeaujot@air-de-ryan.home>
2024-02-13 12:19:06 +0100

    ci.yml

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_ROOT_PASSWORD,
-  database: process.env.MYSQL_DB,
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "innovin",
 });
 
 // Ajout du message de connexion réussie à la console
